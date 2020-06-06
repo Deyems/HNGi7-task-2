70b0f4704bdd730ec5fce5ec4f3ac7f39190f8e6
Adeyemi sola <45840438+Deyems@users.noreply.github.com>
2020-06-06 12:24:59 +0000

    Update Deyems-HNG-05272.js
    I changed from using classes to simple variable declarations th

--- scripts/Deyems-HNG-05272.js
+++ scripts/Deyems-HNG-05272.js
@@ -1,14 +1,8 @@
-class hngTask{
-  constructor(fullname,id,email,language){
-    this.fullname = fullName;
-    this.id = id;
-    this.email = email;
-    this.language = language;
-  }
-  showDetails(){
-    return `Hello World, this is ${this.fullName} with HNGi7 ID ${this.id} and email ${this.email} using ${this.language} for stage 2 task`;
-  }
-}
 
-let task2 = new hngTask('Adeyemi Adesola','HNG-05272','adeyemisola11@gmail.com','Javascript');
-console.log(task2.showDetails());
+   const fullName = "Adeyemi Adesola";
+   const id = "HNG-05272";
+   const email = "adeyemisola11@gmail.com";
+   const language = "Javascript";
+  
+  
+    console.log(`Hello World, this is ${fullName} with HNGi7 ID ${id} and email ${email} using ${language} for stage 2 task`);
